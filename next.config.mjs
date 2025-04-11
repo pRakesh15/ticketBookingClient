8a3a86e693f46379679994b6df019ffc14287f94
yiconic <rp5865442@gmail.com>
2025-04-12 00:21:12 +0530

    add the config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // swcMinify: true,
+    swcMinify: true,
     images: {
       domains: ['localhost'],
     },
